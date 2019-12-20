8131ab57883dcd42af492e9fe9a8cbc0660ee49a
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2019-12-20 11:48:50 -0300

    Update EmailNotifier.java

--- src/main/java/br/com/palerique/algafood/notifier/EmailNotifier.java
+++ src/main/java/br/com/palerique/algafood/notifier/EmailNotifier.java
@@ -1,6 +1,7 @@
 package br.com.palerique.algafood.notifier;
 
 import br.com.palerique.algafood.model.Customer;
+import java.util.Locale;
 
 public class EmailNotifier implements Notifier {
 
@@ -9,16 +10,17 @@ public class EmailNotifier implements Notifier {
 
   public EmailNotifier(String smtpHost) {
     this.smtpHost = smtpHost;
+    System.out.printf("SMTP host: %s%n", smtpHost);
   }
 
   @Override
   public void notify(Customer customer, String message) {
 
     if (upperCase) {
-      message = message.toUpperCase();
+      message = message.toUpperCase(Locale.ENGLISH);
     }
 
-    System.out.printf("Notificando %s através do e-mail %s: %s\n",
+    System.out.printf("Notificando %s através do e-mail %s: %s%n",
         customer.getName(), customer.getEmail(), message);
   }
 
